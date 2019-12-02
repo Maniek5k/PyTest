2f50ad92a512f8d9e011aab8c083b55ee0b363c5
Michal Kasprzyk <emailer5k@gmail.com>
2019-12-02 18:45:59 +0100

    Add contact form tests

---
+++ tests/contact/test_contact.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+
+from conftest import Base
+from conftest import services
+from pages.page_contact import contact
+
+
+class TestContact(Base):
+    def test_contact(self):
+        services.assert_and_click(self, By.LINK_TEXT, contact.contact_link)
+
+        services.send_keys_by_xpath(self, contact.contact_name, contact.contact_data_name)
+
+        services.send_keys_by_xpath(self, contact.contact_mail, contact.contact_data_mail)
+
+        services.send_keys_by_xpath(self, contact.contact_message, contact.contact_data_short_message)
+
+        services.assert_and_click(self, By.XPATH, contact.contact_submit)
+
+        services.assert_text(self, By.XPATH, contact.contact_success, contact.contact_success_text)
---
+++ tests/contact/test_contact_wrong_mail.py
@@ -0,0 +1,20 @@
+from selenium.webdriver.common.by import By
+
+from conftest import Base
+from conftest import services
+from pages.page_contact import contact
+
+
+class TestContactWrongMail(Base):
+    def test_contact(self):
+        services.assert_and_click(self, By.LINK_TEXT, contact.contact_link)
+
+        services.send_keys_by_xpath(self, contact.contact_name, contact.contact_data_name)
+
+        services.send_keys_by_xpath(self, contact.contact_mail, contact.contact_data_name)
+
+        services.send_keys_by_xpath(self, contact.contact_message, contact.contact_data_message)
+
+        services.assert_and_click(self, By.XPATH, contact.contact_submit)
+
+        services.assert_text(self, By.CSS_SELECTOR, contact.contact_danger, contact.contact_danger_mail_text)
